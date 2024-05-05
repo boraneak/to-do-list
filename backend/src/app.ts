3aba9f06a558f6668f7346ed4132b5e30cebd95c
boraneak <boraneak123@gmail.com>
2024-05-05 14:50:18 +0700

    add nodemon & morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import "dotenv/config";
+import morgan from "morgan";
 import { connectToDatabase } from "../utils/database";
 const app = express();
 const port = process.env.PORT;
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
