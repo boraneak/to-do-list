20eb2361e35b35be9c76338b813098a8659bcba0
boraneak <boraneak123@gmail.com>
2024-05-10 19:30:31 +0700

    update not found

--- backend/middlewares/notFound.ts
+++ backend/middlewares/notFound.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
-export function notFound(req: Request, res: Response, next: NextFunction) {
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
   res.status(404);
   const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
   next(error);
-}
+};
